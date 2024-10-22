268a9367e194c7ec9d89edc0f212b207bfa44297
Atul Singh <73229173+sekharatulsingh@users.noreply.github.com>
2024-10-22 12:43:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,38 @@
-provider "aws" {
-  region = "us-east-1"
+provider "azurerm" {
+  features {}
 }
 
-resource "aws_eks_cluster" "weather_cluster" {
-  name     = "weather-app-cluster"
-  role_arn = aws_iam_role.eks_role.arn
-  vpc_config {
-    subnet_ids = aws_subnet.public.*.id
+resource "azurerm_resource_group" "weather_rg" {
+  name     = "weather-app-rg"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "weather_cluster" {
+  name                = "weather-app-cluster"
+  location            = azurerm_resource_group.weather_rg.location
+  resource_group_name = azurerm_resource_group.weather_rg.name
+  dns_prefix          = "weatherapp"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
   }
 }
 
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.weather_cluster.kube_config_raw
+}
+
 output "cluster_endpoint" {
-  value = aws_eks_cluster.weather_cluster.endpoint
+  value = azurerm_kubernetes_cluster.weather_cluster.fqdn
 }
